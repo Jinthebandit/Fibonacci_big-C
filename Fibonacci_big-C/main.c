f489ff7e0cff8befc69f7767d6817738033217a1
Jinthebandit <jinthebandit@gmail.com>
2019-12-01 06:06:47 +0100

    initiate

---
+++ Fibonacci_big-C/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jinth
+ *
+ * Created on 1. Dezember 2019, 06:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
